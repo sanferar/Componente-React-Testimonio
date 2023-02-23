b9c23c1690012fa710ce30a65044e8695f11e5e6
santi <santi@sanferar.com>
2023-02-23 17:12:38 +0100

    Añadido la importacion del componenete Testimonio al archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Testimonio } from './componenetes/Testimonio';
 function App() {
   return (
     <div className="App">
